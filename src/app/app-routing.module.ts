96cb7efe8cf38d906bcfb73e0981384b1bfaebf7
rohitprashar <rohitprashar21@gmail.com>
2020-08-04 13:36:01 +0530

    userForm
    UserForm chnages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeViewComponent } from './view/home-view/home-view.component';
 import { LoginViewComponent } from './view/login-view/login-view.component';
 import { ServiceDetailComponent } from './view/service-detail/service-detail.component';
+import { UserformComponent } from './view/userform/userform.component';
 
 
 const routes: Routes = [
   { path: 'home', component: HomeViewComponent },
   { path: 'login', component: LoginViewComponent },
   { path: 'detail/:id', component: ServiceDetailComponent },
+  { path: 'userform', component:UserformComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' }
 ];
 
